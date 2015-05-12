75bc23b2f3b54ef326d99e6b8c1b2e7a65207750
minsikzzang <minsikzzang@gmail.com>
2015-05-12 22:54:17 +0100

    Add pages in site content

--- app/models/site.rb
+++ app/models/site.rb
@@ -9,6 +9,7 @@ class Site < ActiveRecord::Base
   def to_h
     # descriptions = self.description.split("\n").reject{ |n| n == "" }
     hash = self.attributes.merge({ business: business.to_h,
+                                   pages: pages
                             # descriptions: descriptions,
                             # content1_title: "Coffee (CONTENT TITLE1)",
                             # content1_desc: "We use coffee from Cup in Woolloongabba. They roast a seasonal blend just for us and we also offer single origin espresso from them. (CONTENT_DESC1)",
