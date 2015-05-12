b223141cb7a08dba3c54ea11a839fe1252164a4c
minsikzzang <minsikzzang@gmail.com>
2015-05-12 01:01:44 +0100

    Add page update

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   post 'sites/:site_id/pages', to: 'pages#create'
   get 'sites/:site_id/pages', to: 'pages#index'
   get 'pages/:id', to: 'pages#show'
-  put 'pages/:id', to: 'pages#update'
+  patch 'pages/:id', to: 'pages#update'
 end
