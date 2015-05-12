6d9a354efe0ae5ba1009fb1d746903c24b0ee20f
minsikzzang <minsikzzang@gmail.com>
2015-05-12 22:59:18 +0100

    Add sort for contents

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,4 +1,8 @@
 class Page < ActiveRecord::Base
   belongs_to :site
   has_many :contents
+
+  def to_h
+    hash = self.attributes.merge({ contents: contents.sort { |a,b| a.order <=> b.order })
+  end
 end
