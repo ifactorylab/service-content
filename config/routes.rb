86b6ebdb738b5d3e99e425e7c511d49c63b7446e
minsikzzang <minsikzzang@gmail.com>
2015-05-14 21:15:24 +0100

    Change put to patch for upload

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,10 @@ Rails.application.routes.draw do
   get 'sites/:site_id/pages', to: 'pages#index'
   get 'pages/:id', to: 'pages#show'
   patch 'pages/:id', to: 'pages#update'
-  put 'pages/:id/background', to: 'pages#upload'
+  patch 'pages/:id/background', to: 'pages#upload'
 
   get 'pages/:page_id/contents', to: 'contents#index'
   patch 'contents/:id', to: 'contents#update'
-  put 'contents/:id/image', to: 'contents#upload'
+  patch 'contents/:id/image', to: 'contents#upload'
 
 end
