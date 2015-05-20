a7ebb23e5f83341970100c2ce1da0ee4f88147ea
minsikzzang <minsikzzang@gmail.com>
2015-05-21 00:06:47 +0100

    Add auth gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::API
+  include GemAuth
+
   rescue_from ActiveRecord::RecordNotFound, :with => :not_found_exception
 
   def not_found_exception
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
   before_action :set_site, :only => :index
   before_action :set_page, :only => [:update, :upload]
+  before_action :authorize_partner
 
   # curl -v -XGET localhost:3000/sites/2a014646-ca54-4cdb-b004-76be3383cf0b/pages -H 'Venice-Authorization: cee3eb61-2435-4a28-b422-9ebfacc8dbec'
   def index
