642642ba8c2ab0281b489fa7de2e702c313659c0
minsikzzang <minsikzzang@gmail.com>
2015-07-15 23:03:52 +0100

    Add content when only for booking / welcome pages

--- app/models/page.rb
+++ app/models/page.rb
@@ -7,27 +7,35 @@ class Page < ActiveRecord::Base
 
   def to_h
     if contents.count == 0
-      # If there's no page exist, we need to generate them
-      contents << Content.create(
-        :page_id => id,
-        :order => 0,
-        :title => site.name,
-        :description => site.description)
-      contents << Content.create(
-        :page_id => id,
-        :order => 1,
-        :title => "Coffee",
-        :description => "We use coffee from Cup in Woolloongabba. They roast a seasonal blend just for us and we also offer single origin espresso from them.")
-      contents << Content.create(
-        :page_id => id,
-        :order => 2,
-        :title => "Baking",
-        :description => "We bake every morning and will always have fresh cakes, pastries and muffins on hand.")
-      contents << Content.create(
-        :page_id => id,
-        :order => 3,
-        :title => "Food",
-        :description => "Seasonal and simple. Our menu is designed with clean, tasty flavours in mind.")
+      if page.order == 0
+        # If there's no page exist, we need to generate them
+        contents << Content.create(
+          :page_id => id,
+          :order => 0,
+          :title => site.name,
+          :description => site.description)
+        contents << Content.create(
+          :page_id => id,
+          :order => 1,
+          :title => "Coffee",
+          :description => "We use coffee from Cup in Woolloongabba. They roast a seasonal blend just for us and we also offer single origin espresso from them.")
+        contents << Content.create(
+          :page_id => id,
+          :order => 2,
+          :title => "Baking",
+          :description => "We bake every morning and will always have fresh cakes, pastries and muffins on hand.")
+        contents << Content.create(
+          :page_id => id,
+          :order => 3,
+          :title => "Food",
+          :description => "Seasonal and simple. Our menu is designed with clean, tasty flavours in mind.")
+      elsif page.order == 3
+        contents << Content.create(
+          :page_id => id,
+          :order => 0,
+          :title => "RESERVATIONS",
+          :description => "Call  (020) 723-3170 from 9a – 11p daily, or book online.\nReservations required for parties of 4 or more.")
+      end
     end
 
     self.attributes.merge({
