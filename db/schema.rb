a57edfb7fc5a0bf2c2f025ad2fa05f23cafee0e2
minsikzzang <minsikzzang@gmail.com>
2015-08-04 22:10:03 +0100

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150720212624) do
+ActiveRecord::Schema.define(version: 20150804210118) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -176,6 +176,7 @@ ActiveRecord::Schema.define(version: 20150720212624) do
     t.string   "app_url"
     t.text     "description", default: ""
     t.string   "status",      default: "development"
+    t.text     "keywords",    default: ""
   end
 
   create_table "styles", id: :uuid, default: "uuid_generate_v4()", force: :cascade do |t|
